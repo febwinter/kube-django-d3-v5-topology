02dc3933b75c4a72a388ae81d4ee9ecfe1f068b8
febwinter <skyv2080@gmail.com>
2019-10-15 19:22:22 +0900

    commit 2.4

--- kubetop/dashboard/views.py
+++ kubetop/dashboard/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 import json
-from dashBoard.models import KubeFile
+from dashboard.models import KubeFile
 
 # Create your views here.
 
